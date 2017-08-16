c8689a47fff7b4e4dda333bb0914e7526050bb4b
Malcolm Karutz <malcolm_karutz@live.com>
2017-08-16 19:22:23 +1000

    Workshop 3 programming exercises.

--- workshop03/ex07-03.c
+++ workshop03/ex07-03.c
@@ -3,38 +3,47 @@
 
 #define SIZE 1000 
 
-int make_distinct(int A[], int n);
 void print_int_array(int A[], int n);
 int read_int_array(int A[], int size);
-int sort_distinct(int A[], int n);
+int remove_repeats(int A[], int n);
+int sort_reduce_int_array(int A[], int n);
 void sort_int_array(int A[], int n);
 void swap_ints(int *a, int *b);
 
-int 
+int
 main(int argc, char *argv[]) {
     int A[SIZE];
     int n;
 
     n = read_int_array(A, SIZE);
-    n = sort_distinct(A, n);
+    printf("Before: ");
+    print_int_array(A, n);
+
+    n = sort_reduce_int_array(A, n);
+    printf("After: ");
     print_int_array(A, n);
 
     return 0;
 }
 
-int 
+int
 read_int_array(int A[], int size) {
     printf("Enter as many as %d values, ^D to end:\n", size);
     /* Your code here. */
     return 0;
 }
 
+void
+print_int_array(int A[], int n) {
+    /* Your code here. */
+}
+
 /* Sorts an array of integers, removing repeats. Returns the number of distinct 
    values. */
 int
-sort_distinct(int A[], int n) {
+sort_reduce_int_array(int A[], int n) {
     sort_int_array(A, n);
-    return make_distinct(A, n);
+    return remove_repeats(A, n);
 }
 
 void
@@ -57,12 +66,7 @@ swap_ints(int *a, int *b) {
 /* Removes repeated values from a sorted array of integers. The distinct values 
    will remain in sorted order. Returns the number of distinct values. */
 int
-make_distinct(int A[], int n) {
+remove_repeats(int A[], int n) {
     /* Your code here. */
     return 0;
-}
-
-void
-print_int_array(int A[], int n) {
-    /* Your code here. */
 }
--- workshop03/ex07-04.c
+++ workshop03/ex07-04.c
@@ -5,8 +5,6 @@
 
 void print_freq_table(int A[], int size);
 int read_int_array(int A[], int size);
-void sort_int_array(int A[], int n);
-void swap_ints(int *a, int *b);
 
 int 
 main(int argc, char *argv[]) {
@@ -14,7 +12,6 @@ main(int argc, char *argv[]) {
     int n;
 
     n = read_int_array(A, SIZE);
-    sort_int_array(A, n);
     print_freq_table(A, n);
 
     return 0;
@@ -27,26 +24,10 @@ read_int_array(int A[], int size) {
     return 0;
 }
 
-/* Given a sorted array, prints a table of its values and their frequencies. */
+/* Prints a table of the frequencies of values in an array of integers. */
 void
 print_freq_table(int A[], int n) {
     printf("Value    Freq");
     /* Your code here. */
 }
 
-void
-sort_int_array(int A[], int n) {
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = i; j > 0 && A[j] > A[j - 1]; j--) {
-            swap_ints(&A[j], &A[j - 1]);
-        }
-    }
-}
-
-void
-swap_ints(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
