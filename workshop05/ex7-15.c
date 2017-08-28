c944fd53b8dc4a03a18bfd4cfd8c294cc34e4783
Malcolm Karutz <malcolm_karutz@live.com>
2017-08-28 17:30:53 +1000

    Update ex7-15.c

--- workshop05/ex7-15.c
+++ workshop05/ex7-15.c
@@ -9,8 +9,10 @@ int
 main(int argc, char *argv[]) {
     printf("Running tests:\n");
     assert(is_anagram("luster", "result"));
-    assert(is_anagram("disease", "seaside"));
+    assert(is_anagram("programming is fun", "prof margin musing"));
     assert(is_anagram("anything", "anything"));
+    assert(is_anagram("", ""));
+    assert(!is_anagram("", "anything"));
     assert(!is_anagram("something", "something else"));
     printf("All tests passed!\n");
 
