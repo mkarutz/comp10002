8ce7217111c43710ac5671a9f1b6e004a25b7411
Malcolm Karutz <malcolm_karutz@live.com>
2017-08-28 17:27:55 +1000

    Adding test cases to ex7-12.c

--- workshop05/ex7-12.c
+++ workshop05/ex7-12.c
@@ -10,6 +10,8 @@ main(int argc, char *argv[]) {
     printf("Running tests:\n");
     assert(is_palindrome("rats live on no evil star"));
     assert(is_palindrome("racecar"));
+    assert(is_palindrome(""));
+    assert(is_palindrome("a"));
     assert(!is_palindrome("A man, a plan, a canal, Panama!"));
     assert(!is_palindrome("race car"));
     printf("All tests passed!\n");
