824239aa4ec7ecc194e31455939a53d0738d7456
Malcolm Karutz <malcolm_karutz@live.com>
2017-08-28 17:58:33 +1000

    Update ex7-16.c

--- workshop05/ex7-16.c
+++ workshop05/ex7-16.c
@@ -13,27 +13,27 @@ int getword(word_t W, int limit);
 
 int
 main(int argc, char *argv[]) {
-	word_t one_word, all_words[MAXWORDS];
-	int numdistinct=0, totwords=0, i, found;
-	while (getword(one_word, MAXCHARS) != EOF) {
-		totwords = totwords+1;
-		/* linear search in array of previous words... */
-		found = 0;
-		for (i=0; i<numdistinct && !found; i++) {
-			found = (strcmp(one_word, all_words[i]) == 0);
-		}
-		if (!found && numdistinct<MAXWORDS) {
-			strcpy(all_words[numdistinct], one_word);
-			numdistinct += 1;
-		}
-		/* NB - program silently discards words after
-		   MAXWORDS distinct ones have been found */
-	}
-	printf("%d words read\n", totwords);
-	for (i=0; i<numdistinct; i++) {
-		printf("word #%d is \"%s\"\n", i, all_words[i]);
-	}
-	return 0;
+    word_t one_word, all_words[MAXWORDS];
+    int numdistinct=0, totwords=0, i, found;
+    while (getword(one_word, MAXCHARS) != EOF) {
+        totwords = totwords+1;
+        /* linear search in array of previous words... */
+        found = 0;
+        for (i=0; i<numdistinct && !found; i++) {
+            found = (strcmp(one_word, all_words[i]) == 0);
+        }
+        if (!found && numdistinct<MAXWORDS) {
+            strcpy(all_words[numdistinct], one_word);
+            numdistinct += 1;
+        }
+        /* NB - program silently discards words after
+           MAXWORDS distinct ones have been found */
+    }
+    printf("%d words read\n", totwords);
+    for (i=0; i<numdistinct; i++) {
+        printf("word #%d is \"%s\"\n", i, all_words[i]);
+    }
+    return 0;
 }
 
 /* Extract a single word out of the standard input, of not
