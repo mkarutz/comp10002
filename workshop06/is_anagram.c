e55cc42414e3c38a4f813b6ddd6da2de75e0f63f
Malcolm Karutz <malcolm_karutz@live.com>
2017-09-04 13:10:40 +1000

    added string exercises from lecture 6

---
+++ workshop06/is_anagram.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int is_anagram(char *str1, char *str2);
+
+int 
+main(int argc, char *argv[]) {
+    printf("Running tests:\n");
+    assert(is_anagram("Algorithms", "Glamor Hits"));
+    assert(is_anagram("LUSTER", "result"));
+    assert(is_anagram("anything", "any     thing"));
+    assert(is_anagram("", ""));
+    assert(!is_anagram("", "anything"));
+    assert(!is_anagram("something", "something else"));
+    printf("All tests passed!\n");
+
+    return 0;
+}
+
+/* Returns 1 if the two strings contain the same letters, possibly in a 
+   different order, and 0 otherwise, ignoring whitespace characters, and 
+   ignoring case. 
+   
+   For example, is_anagram("Algorithms", "Glamor Hits") should return 1. */
+int 
+is_anagram(char *str1, char *str2) {
+    /* Your code here. */
+    return 0;
+}
---
+++ workshop06/is_subsequence.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+
+int is_subsequence(char *s1, char *s2);
+
+int
+main() {
+    printf("Running tests:\n");
+    assert(is_subsequence("abc", "aaabbbccc"));
+    assert(!is_subsequence("aaabbbccc", "abc"));
+    assert(is_subsequence("abc", "abc"));
+    assert(is_subsequence("bee", "abbreviate"));
+    assert(!is_subsequence("bee", "acerbate"));
+    printf("All tests passed!\n");
+
+    return 0;
+}
+
+/* Returns 1 if the characters in s1 appear within s2 in the same order as they 
+   appear in s1. 
+   
+   For example, is_subsequence("bee", "abbreviate") should be 1, whereas 
+   is_subsequence("bee", "acerbate") should be 0.*/
+int 
+is_subsequence(char *s1, char *s2) {
+    /* Your code here. */
+    return 0;
+}
