c85de2110b30973dcda16fa46d8a57e3a986cdb1
Malcolm Karutz <malcolm_karutz@live.com>
2017-09-18 14:13:39 +1000

    added skeleton programs for workshop 10

---
+++ workshop10/country.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Your code here: give declarations that reflect the data scenario that is 
+   described. */
+
+int calculate_avg_age_death(person_t *country, int num_people);
+int calculate_tax_revenue(person_t *country, int num_people, int year);
+
+int 
+main(int argc, char *argv[]) {
+    /* Your code here: write some tests for your functions. */
+    return 0;
+}
+
+/* Calculates, for a specified country, the average age of death. Does not 
+   include people that are not yet dead. */
+int 
+calculate_avg_age_death(person_t *country, int num_people) {
+    /* Your code here. */
+    return 0;
+}
+
+/* Calculates, for a specified country, the total taxation revenue in a 
+   specified year. */
+int 
+calculate_tax_revenue(person_t *country, int num_people, int year) {
+    /* Your code here. */
+    return 0;
+}
---
+++ workshop10/stringdupe.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *string_dupe(char *s);
+char **string_set_dupe(char **S);
+void string_set_free(char **S);
+
+int
+main(int argc, char *argv[]) {
+    /* Your code here. */
+    return 0;
+}
+
+/* Creates a copy of the string s and returns a pointer to it. */
+char*
+string_dupe(char *s) {
+    /* Your code here. */
+    return NULL;
+}
+
+/* Creates a copy of the string-set S and returns a pointer to it. */
+char**
+string_set_dupe(char **S) {
+    /* Your code here. */
+    return NULL;
+}
+
+/* Releases the memory associated with the string-set S. */
+void 
+string_set_free(char **S) {
+    /* Your code here. */
+}
