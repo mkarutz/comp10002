f3725d80a4548acc59a6d63665f78d93010ee5b0
Malcolm Karutz <malcolm_karutz@live.com>
2018-03-28 11:24:45 +1100

    Updated 7.8

--- workshop04/ex07-08.c
+++ workshop04/ex07-08.c
@@ -5,27 +5,19 @@
 
 int kth_smallest(int A[], int n, int k);
 int read_int_array(int A[], int maxvals);
-int read_k(int n);
 
 int 
 main(int argc, char *argv[]) {
 	int A[SIZE];
 	int n, k;
 
-	/* Read in the value for k. */
-	k = read_k(SIZE);
-
 	/* Read in the array. */
 	n = read_int_array(A, SIZE);
 	
-	/* Check that k and n are valid. */
-	if (n <= k) {
-	    printf("Error: not enough integers for the given value of k.\n");
-	    exit(EXIT_FAILURE);
+	/* Try some values of k. */
+	for (k = 0; k < n; k++) {
+	    printf("The %2dth smallest value is %3d.\n", k, kth_smallest(A, n, k));
 	}
-	
-	/* Print the kth smallest value. */
-	printf("The kth smallest value is %d.\n", kth_smallest(A, n, k));
 
 	return 0;
 }
@@ -35,18 +27,6 @@ read_int_array(int A[], int maxvals) {
 	printf("Enter as many as %d values, ^D to end:\n", maxvals);
 	/* YOUR CODE HERE. */
 	return 0;
-}
-
-/* Reads a value between 0 and n - 1. */
-int
-read_k(int n) {
-	int k;
-	printf("Enter a value for k between 0 and %d: ", n - 1);
-	if (scanf("%d", &k) != 1 || k < 0 || k > n - 1) {
-		printf("Invalid input for k.\n");
-		exit(EXIT_FAILURE);
-	}
-	return k;
 }
 
 /* Gets the kth smallest value in the array. */
